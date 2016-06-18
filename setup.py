fb7df0efc7b1f8a391da10bf3a5bbe97e82bcbed
csaez <cesarte@gmail.com>
2016-06-19 04:57:53 +1000

    python packaging (minimal)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="mscreen",
+    version="0.1.0",
+    py_modules=["mscreen"],
+    url="http://github.com/csaez/mscreen",
+    author="Cesar Saez",
+    author_email="hi@cesarsaez.me",
+)
